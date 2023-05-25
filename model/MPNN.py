b5f056bf507c029e4f2e02d212754922b785253c
zhangylch <ylzustc@mail.ustc.edu.cn>
2023-05-26 03:45:29 +0800

    regular commit

--- model/MPNN.py
+++ model/MPNN.py
@@ -16,67 +16,63 @@ class MPNN(torch.nn.Module):
         self.norbital=norbital
         self.nangular=(self.max_l+1)*(self.max_l+1)
         # add the input neuron for each neuron
-        self.index_l=torch.zeros(self.nangular,device=device,dtype=torch.long)
-        for l in range(max_l+1):
-            self.index_l[l*l:(l+1)*(l+1)]=l
         emb_nl.insert(0,1)
         iter_nl.insert(0,self.norbital)
         nl.insert(0,self.norbital)
 
-        self.contracted_coeff=nn.parameter.Parameter(torch.nn.init.xavier_uniform_(torch.randn(max_l+1,nwave,norbital)))
+        self.contracted_coeff=nn.parameter.Parameter(torch.nn.init.xavier_uniform_(torch.randn(nwave,norbital)))
 
-        init_coeff=torch.ones(nwave,device=device,dtype=Dtype)
-        alpha=torch.ones(nwave,device=device,dtype=Dtype)
-        rs=(torch.rand(nwave)*np.sqrt(cutoff)).to(device)
-        initbias=torch.hstack((init_coeff,rs,alpha))
+        initbias=torch.ones(nwave,device=device,dtype=Dtype)/maxneigh
+        alpha=(torch.rand(nwave)+0.2).to(device)
+        rs=(torch.rand(nwave)*cutoff).to(device)
+        initbias=torch.hstack((initbias,alpha,rs))
         # embedded nn
-        self.embnn=MLP.NNMod(self.nwave*3,emb_nblock,emb_nl,np.array([0]),actfun,initbias=initbias,layernorm=layernorm)
+        self.embnn=MLP.NNMod(self.nwave*3,emb_nblock,emb_nl,np.array([0]),actfun,initbias=initbias,layernorm=emb_layernorm)
 
         # instantiate the nn radial function and disable the dropout 
         self.sph_cal=sph_cal.SPH_CAL(max_l,device=device,Dtype=Dtype)
         itermod=OrderedDict()
         for i in range(iter_loop):
+            initbias=torch.ones(nwave,device=device,dtype=Dtype)
             f_iter="memssage_"+str(i)
-            itermod[f_iter]= MLP.NNMod(nwave,iter_nblock,iter_nl,iter_dropout_p,actfun,initbias=torch.ones(nwave),layernorm=iter_layernorm)
+            itermod[f_iter]= MLP.NNMod(nwave,iter_nblock,iter_nl,iter_dropout_p,actfun,initbias=initbias,layernorm=iter_layernorm)
         self.itermod= torch.nn.ModuleDict(itermod)
         self.outnn=MLP.NNMod(1,nblock,nl,dropout_p,actfun,initbias=torch.tensor(np.array([initpot])),layernorm=layernorm)
 
     def forward(self,cart,neighlist,shifts,center_factor,neigh_factor,species):
         distvec=cart[neighlist[1]]-cart[neighlist[0]]+shifts
         distances=torch.linalg.norm(distvec,dim=1)
+        distvec=distvec.T/distances
         center_coeff=self.embnn(species)
         full_center_list=center_coeff[neighlist[0]]
-        neigh_emb=(full_center_list*center_coeff[neighlist[1]]).T
+        neigh_emb=full_center_list*center_coeff[neighlist[1]]
         cut_distances=neigh_factor*self.cutoff_cosine(distances)  
-        distvec=torch.einsum("ik,i ->ik",distvec,cut_distances)
-        contracted_coeff=self.contracted_coeff[self.index_l]
         # for the efficiency of traditional ANN, we do the first calculation of density mannually.
-        weight_distvec=torch.einsum("ji,ji,ik->ikj",neigh_emb[:self.nwave],self.radial_func(distances,neigh_emb[self.nwave:self.nwave*2],neigh_emb[self.nwave*2:self.nwave*3]),distvec)
-        wc_distvec=cart.new_zeros((cart.shape[0],3,self.nwave))
-        wc_distvec=torch.index_add(wc_distvec,0,neighlist[0],weight_distvec)
-        sph=self.sph_cal(wc_distvec.permute(1,0,2))
-        contracted_sph=torch.einsum("kij,kjm->ikm",sph,contracted_coeff)
-        density=torch.einsum("ikm,ikm->im",contracted_sph,contracted_sph)
+        radial_func=torch.einsum("i,ij->ij",cut_distances,self.radial_func(distances,neigh_emb[:,self.nwave:self.nwave*2],neigh_emb[:,self.nwave*2:]))
+        sph=self.sph_cal(distvec)
+        orbital=torch.einsum("ij,ij,ki->ikj",radial_func,neigh_emb[:,:self.nwave],sph)
+        center_orbital=torch.zeros((cart.shape[0],self.nangular,self.nwave),dtype=cart.dtype,device=cart.device)
+        center_orbital=torch.index_add(center_orbital,0,neighlist[0],orbital)
+        contracted_orbital=torch.einsum("ikj,jm->ikm",center_orbital,self.contracted_coeff)
+        density=torch.einsum("ikm,ikm->im",contracted_orbital,contracted_orbital)
         for iter_loop, (_, m) in enumerate(self.itermod.items()):
             iter_coeff=m(density)[neighlist[1]]
-            iter_density,wc_distvec=self.density(weight_distvec,cut_distances,iter_coeff,neighlist[0],neighlist[1],contracted_coeff,wc_distvec)
+            iter_density,center_orbital=self.density(orbital,cut_distances,iter_coeff,neighlist[0],neighlist[1],center_orbital)
             # here cente_coeff is for discriminating for the different center atoms.
             density=density+iter_density
         output=self.outnn(density)
         return torch.einsum("ij,i ->",output,center_factor)
 
-    def density(self,weight_distvec,cut_distances,iter_coeff,index_center,index_neigh,contracted_coeff,wc_distvec):
-        wn_distvec=torch.einsum("i,ikj -> ikj",cut_distances,wc_distvec[index_neigh])
-        weight_distvec=torch.einsum("ij,ikj -> ikj",iter_coeff,weight_distvec)+wn_distvec
-        wc_distvec=torch.index_add(wc_distvec,0,index_center,weight_distvec)
-        sph=self.sph_cal(wc_distvec.permute(1,0,2))
-        contracted_sph=torch.einsum("kij,kjm->ikm",sph,contracted_coeff)
-        density=torch.einsum("ikm,ikm->im",contracted_sph,contracted_sph)
-        return density,wc_distvec
+    def density(self,orbital,cut_distances,iter_coeff,index_center,index_neigh,center_orbital):
+        weight_orbital=torch.einsum("ij,ikj -> ikj",iter_coeff,orbital)+torch.einsum("ikj,i->ikj",center_orbital[index_neigh],cut_distances)
+        center_orbital=torch.index_add(center_orbital,0,index_center,weight_orbital)
+        contracted_orbital=torch.einsum("ikj,jm->ikm",center_orbital,self.contracted_coeff)
+        density=torch.einsum("ikm,ikm->im",contracted_orbital,contracted_orbital)
+        return density,center_orbital
      
     def cutoff_cosine(self,distances):
         tmp=0.5 * torch.cos(distances * (np.pi / self.cutoff)) + 0.5
         return tmp*tmp
 
-    def radial_func(self,distances,rs,alpha):
-        return torch.exp(-torch.square(alpha*(distances-rs)))
+    def radial_func(self,distances,alpha,rs):
+        return torch.exp(-torch.square(alpha*(distances[:,None]-rs)))
